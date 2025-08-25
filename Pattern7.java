520112f9c76e601635ab26a60b9a7538ecc1db4d
Pavan Sathvik <117331663+PavanSathvik1@users.noreply.github.com>
2025-08-25 21:13:37 +0530

    Upload Pattern7

---
+++ Pattern7.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++)
+    {
+        for (int j =0; j<n-i-1; j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=0;j< i+i+1;j++){
+            
+            System.out.print("*");
+        }
+         for (int j =0; j<n-i-1; j++)
+        {
+            System.out.print(" ");
+        }
+        System.out.println();
+        scan.close();
+        }
+    }
+}
